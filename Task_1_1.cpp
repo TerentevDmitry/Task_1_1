450e399decf41628b793d374689d38f03c37f962
TerentevDmitry <Zabey52@gmail.com>
2023-12-25 10:36:51 +0300

    Добавьте файлы проекта.

---
+++ Task_1_1.cpp
@@ -0,0 +1,41 @@
+﻿// Домашнее задание к занятию «Многопоточность»
+// Задание 1.Очередь клиентов
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <functional>
+
+
+void clientThread(int* maxClients, int* clientCounter, std::mutex* mtx) {
+    while (*clientCounter < *maxClients) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::lock_guard<std::mutex> lock(*mtx);
+        clientCounter++;
+        std::cout << "Client joined, total clients: " << clientCounter << std::endl;
+    }
+}
+
+void operatorThread() {
+    while (clientCounter > 0) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::lock_guard<std::mutex> lock(mtx);
+        clientCounter--;
+        std::cout << "Client served, total clients: " << clientCounter << std::endl;
+    }
+}
+
+int main() 
+{
+    int maxClients = 7;
+    int clientCounter = 0;
+    std::mutex mtx;
+
+    std::thread client(clientThread(&maxClients, &clientCounter, &mtx));
+    std::thread operatorT(operatorThread);
+
+    client.join();
+    operatorT.join();
+
+    return 0;
+}
